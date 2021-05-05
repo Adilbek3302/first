5f98bcd83003a5b15600dec84a0b745f56399930
Adilbek3302 <83700607+Adilbek3302@users.noreply.github.com>
2021-05-05 15:04:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// возраст
+var a = prompt('введите свое имя');
+console.log('имя: '+a );
+var b = +prompt('введите свой год рождения');
+console.log('год рождения: '+b );
+var c = +prompt('введите нынешний год');
+console.log('нынешний год: '+c );
+console.log( a+' вам '+ (c-b )+' лет');
+alert('нажмите F12')
+
+
